e67aeadad53843ccadb532f7e60467b241b218a1
Aayush Gauutam <professional.atus6693@gmail.com>
2024-08-26 23:28:04 +0545

    sql operators

--- intro.sql
+++ intro.sql
@@ -16,3 +16,22 @@ FROM employees;
 SELECT CONCAT(emp_no, ' is a ', title) AS "Employee Title"
 FROM titles;
 
+-- "" => column and table , space , First letter capital
+-- '' => string
+
+-- WHERE (..AND..AND..) OR (..AND..AND..)
+
+-- precedence => Not- right => and - left => or- left
+
+-- "IS" => filter null , not null, true or false
+
+-- sum(coalesce(age,200)) // null ko thau ma 200 aauxa
+
+/* 
+in sql if hamilai two ota check garna xa bhane we use OR
+instead of AND e.g
+age = 2 or age = 4 but we cant say age = 2 and age = 4 coz
+the age cant be 2 and 4 at the same time , of a person
+
+..... WHERE (age is null) or (age is not null)
+*/
