d8267d48dde6861383ed819a768529d1d1b440b3
Aayush Gauutam <professional.atus6693@gmail.com>
2024-08-28 23:15:09 +0545

    qns ans

---
+++ qnsAns.sql
@@ -0,0 +1,20 @@
+
+-- * Question: How many people's zipcode have a 2 in it?.
+select count(customerid) from customers where zip::text
+ilike '%2%'
+
+--  Question: How many people's zipcode start with 2 with the 3rd character being a 1.
+select count(customerid) from customers where zip::text
+like '2_1%'
+
+-- * Question: Which states have phone numbers starting with 302?
+-- * Replace null values with "No State" 
+select COALESCE(address2,'no state') as Address2 from customers where phone::text like '302%'
+
+--* Question: What unique titles do we have?
+select count(DISTINCT title)from titles
+
+-- * Question: Can I get a list of distinct life expectancy ages
+-- * Make sure there are no nulls
+-- */
+select DISTINCT lifeexpectancy from country where lifeexpectancy IS NOT null 
